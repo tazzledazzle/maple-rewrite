ee4e0f30705892598e3511783faa5d6be4df7626
Terence Schumacher <terenceschumacher@gmail.com>
2025-07-21 18:32:47 -0700

    created local project builder, bom parser tests and local project builder tests

---
+++ maple/src/main/kotlin/com/tazzledazzle/internal/LocalProjectBuilder.kt
@@ -0,0 +1,12 @@
+package com.tazzledazzle.internal
+
+class LocalProjectBuilder {
+    // This class is currently empty, but can be used for building local projects in the future.
+    // For example, you could add methods to compile code, run tests, package applications, etc.
+    // Placeholder for future project building code.
+    // This could include methods for:
+    // - Compiling source code
+    // - Running tests
+    // - change version numbers in Gradle files
+    // - Generating build notes or run notes
+}
---
+++ maple/src/test/kotlin/com/tazzledazzle/LocalProjectBuilderTest.kt
@@ -0,0 +1,12 @@
+package com.tazzledazzle
+
+class LocalProjectBuilderTest {
+
+    // This class is currently empty, but can be used for testing local project building functionality in the future.
+    // For example, you could add methods to build a project locally, run tests, etc.
+    // Placeholder for future local project building code.
+    // need to build a project locally
+    // run tests
+    // check build artifacts
+    // validate project structure
+}
